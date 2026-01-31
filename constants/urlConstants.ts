1bb40ce931c948b4c0228331d925249d61426a47
vinoth.vasu <vinoth@testlio.com>
2026-01-31 09:03:58 +0530

    added github workflow file and TC003

--- constants/urlConstants.ts
+++ constants/urlConstants.ts
@@ -1,3 +1,5 @@
 export enum URLConstants {
-    adminURL = "https://apps.theauto-mate.com/crm"
+    adminURL = "https://apps.theauto-mate.com/crm",
+    crm_username = "automate_crm",
+    crm_password = "....."
 }
